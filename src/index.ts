91902f5f73af9433bf471003a46d48c64e7a3dc9
RyosukeSomeya <rx.78.0618@gmail.com>
2021-10-07 23:52:06 +0900

    classに型を実装

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,58 @@
+interface myObj {
+  [key: string]: string;
+}
 class ObjectWrapper {
-    private _obj;
+      private _obj: myObj;
     /***
      * 引数のオブジェクトのコピーを this._objに設定
      */
-    constructor(_obj: Object) {}
+    constructor(_obj: myObj) {
+      this._obj = _obj;
+    }
     /**
      * this._objのコピーを返却
      * @return Object
      */
-    get obj() {}
+    get obj(): myObj {
+      return this._obj;
+    }
 
     /**
      * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
      * @param key オブジェクトのキー
      * @param val オブジェクトの値
      */
-    set(key, val): boolean {}
+    set(key: string, val: string): boolean {
+      if (key in this._obj) {
+        this._obj[key] = val;
+        return true;
+      } else {
+        return false;
+      }
+    }
 
     /**
      * 指定したキーの値のコピーを返却
      * 指定のキーが存在しない場合 undefinedを返却
      * @param key オブジェクトのキー
      */
-    get(key) {}
+    get(key: string): string | undefined {
+      if (key in this._obj) {
+        return this._obj[key];
+      } else {
+        return undefined;
+      }
+    }
 
     /**
      * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
      */
-    findKeys(val: unknown) {}
+    findKeys(val: unknown): string[] {
+      const result: string[] = Object.keys(this._obj).filter((key: string):boolean => {
+        return this._obj[key] === val;
+      });
+      return result;
+    }
   }
 
   /**
@@ -36,13 +61,13 @@ class ObjectWrapper {
    */
   const obj1 = { a: '01', b: '02' };
   const wrappedObj1 = new ObjectWrapper(obj1);
-  
+
   if (wrappedObj1.obj.a === '01') {
     console.log('OK: get obj()');
   } else {
     console.error('NG: get obj()');
   }
-  
+
   if (
     wrappedObj1.set('c', '03') === false &&
     wrappedObj1.set('b', '04') === true &&
@@ -52,7 +77,7 @@ class ObjectWrapper {
   } else {
     console.error('NG: set(key, val)');
   }
-  
+
   if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
     console.log('OK: get(key)');
   } else {
