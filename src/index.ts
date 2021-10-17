5bebe84221c6d244cf113a7529cb24eedb37af80
RyosukeSomeya <rx.78.0618@gmail.com>
2021-10-17 23:33:20 +0900

    差分をmerge:2

--- src/index.ts
+++ src/index.ts
@@ -55,29 +55,12 @@ class ObjectWrapper<T extends Object> {
    * 指定のキーが存在しない場合 undefinedを返却
    * @param key オブジェクトのキー
    */
-<<<<<<< HEAD
   // get(key) {}
-=======
-  get(key: keyof MyObj): string | number {
-    return this.obj[key];
-  }
->>>>>>> ba37fa62eb1d07c3ed4371186c40a24571d3dbbc
 
   /**
    * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
    */
-<<<<<<< HEAD
   // findKeys(val: unknown) {}
-=======
-  findKeys(val: '01' | '02'): KeyArray {
-    const result: KeyArray = (Object.keys(this.obj) as KeyArray).filter(
-      (key: keyof MyObj): boolean => {
-        return this._obj[key] === val;
-      }
-    );
-    return result;
-  }
->>>>>>> ba37fa62eb1d07c3ed4371186c40a24571d3dbbc
 }
 
 /**
@@ -93,7 +76,6 @@ if (wrappedObj1.obj.a === '01') {
   console.error('NG: get obj()');
 }
 
-<<<<<<< HEAD
 if (
   // wrappedObj1.set('c', '03') === false &&
   wrappedObj1.set('b', '04') === true &&
@@ -103,17 +85,6 @@ if (
 } else {
   console.error('NG: set(key, val)');
 }
-=======
-// if (
-//   wrappedObj1.set('c', '03') === false && // obj1が持っていないプロパティだからfalse
-//   wrappedObj1.set('b', '04') === true && // obj1が持っているプロパティなのでtrue、値をセットできる
-//   wrappedObj1.obj.b === '04'
-// ) {
-//   console.log('OK: set(key, val)');
-// } else {
-//   console.error('NG: set(key, val)');
-// }
->>>>>>> ba37fa62eb1d07c3ed4371186c40a24571d3dbbc
 
 // if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
 //   console.log('OK: get(key)');
@@ -121,15 +92,9 @@ if (
 //   console.error('NG: get(key)');
 // }
 
-<<<<<<< HEAD
 // const obj2 = { a: '01', b: '02', bb: '02', bbb: '02' };
 // const wrappedObj2 = new ObjectWrapper(obj2);
 // const keys = wrappedObj2.findKeys('02');
-=======
-const obj2 = { a: '01', b: '02', bb: '02', bbb: '02' };
-const wrappedObj2 = new ObjectWrapper(obj2);
-const keys = wrappedObj2.findKeys('02');
->>>>>>> ba37fa62eb1d07c3ed4371186c40a24571d3dbbc
 // if (
 //   wrappedObj2.findKeys('03').length === 0 &&
 //   keys.includes('b') &&
